c49e9c92706d9a32758eb2cdad43d695572cda3c
ongddree <ongddree@gmail.com>
2022-10-08 16:45:50 +0900

    feat: delete,put,patch 추가

--- src/cats/cats.route.ts
+++ src/cats/cats.route.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-//* Read 고양이 정보 전체 조회
+//* READ 고양이 정보 전체 조회
 router.get('/cats', (req, res) => {
   try {
     const cats = Cat;
@@ -21,7 +21,7 @@ router.get('/cats', (req, res) => {
   }
 });
 
-//* Read 특정 고양이 데이터 조회
+//* READ 특정 고양이 데이터 조회
 router.get('/cats/:id', (req, res) => {
   try {
     const { id } = req.params;
@@ -61,4 +61,78 @@ router.post('/cats', (req, res) => {
   }
 });
 
+//* UPDATE 고양이 데이터 업데이트 -> PUT
+router.put('/cats/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === id) {
+        cat = body;
+        result = cat;
+      }
+    });
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* UPDATE 고양이 데이터 부분 업데이트 -> PATCH
+router.patch('/cats/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === id) {
+        cat = { ...cat, ...body };
+        result = cat;
+      }
+    });
+
+    res.status(200).send({
+      success: true,
+      data: {
+        cat: result,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* DELETE 고양이 데이터 삭제 -> DELETE
+
 export default router;
+router.delete('/cats/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const newCats = Cat.filter((cat) => cat.id !== id);
+
+    res.status(200).send({
+      success: true,
+      data: newCats,
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
