0c1bd059c21c9d6211685a425190bd5f9a66b769
ongddree <ongddree@gmail.com>
2022-10-08 15:45:50 +0900

    feat: 고양이 상세 조회 및 고양이 생성 미들웨어 추가

--- src/app.ts
+++ src/app.ts
@@ -4,35 +4,77 @@ import { Cat, CatType } from './app.model';
 //express의 인스턴스. app이 서버 역할
 const app: express.Express = express();
 
-//전체 미들웨어
+//* logging middleware
 app.use((req, res, next) => {
-  console.log('실행');
-  //라우터 찾기
+  console.log(req.rawHeaders[1]);
+  console.log('this is logging middleware');
   next();
 });
 
-//특정 라우터의 미들웨어
-app.get(
-  '/cats/blue',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    next();
-  }
-);
+//* json middleware
+app.use(express.json());
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send({ cats: Cat });
+//* Read 고양이 정보 전체 조회
+app.get('/cats', (req, res) => {
+  try {
+    const cats = Cat;
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get(
-  '/cats/blue',
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send({ blue: Cat[0] });
+//* Read 특정 고양이 데이터 조회
+app.get('/cats/:id', (req, res) => {
+  try {
+    const { id } = req.params;
+    const cat = Cat.find((cat) => {
+      return cat.id === id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
   }
-);
+});
 
-//에러 미들웨어
+//* CREATE 새로운 고양이 추가 api
+app.post('/cats', (req, res) => {
+  try {
+    const data = req.body;
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data: {
+        data,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* 404 middleware
 app.use((req, res, next) => {
-  //라우터 찾기
+  console.log('this is error middleware');
   res.send({ error: '404 not found error' });
 });
 
