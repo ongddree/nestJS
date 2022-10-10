785eb6825d5b3a56994540fd014c5446858992ed
ongddree <ongddree@gmail.com>
2022-10-10 16:09:02 +0900

    feat: controller 패턴

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('hello/:id/:name')
+  getHello(
+    @Req() req: Request,
+    @Body() Body,
+    @Param() param: { id: string; name: string },
+  ): string {
     return this.appService.getHello();
   }
 }
