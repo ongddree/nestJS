1c24e45ca5378805cf77895d5be8b0e824b9ec19
ongddree <ongddree@gmail.com>
2022-10-17 20:24:01 +0900

    feat: LoggerMiddleware 추가

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -2,11 +2,10 @@ import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller('cats')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/:id/:name')
+  @Get()
   getHello(
     @Req() req: Request,
     @Body() Body,
